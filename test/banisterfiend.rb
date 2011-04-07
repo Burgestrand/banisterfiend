68b9748cb02ebe574e583e20ab639a6eee59989f
Kim Burgestrand <kim@burgestrand.se>
2011-04-07 14:45:54 +0200

    Improve test to no longer be random (what was I thinking? :()

--- test/banisterfiend.rb
+++ test/banisterfiend.rb
@@ -1,12 +1,26 @@
 require 'minitest/autorun'
+require 'minitest/mock'
 require 'minitest/spec'
 require 'banisterfiend'
 
+module Kernel
+  def suppress_warnings
+    old_verbose, $VERBOSE = $VERBOSE, nil
+    yield
+  ensure
+    $VERBOSE = old_verbose
+  end
+end
+
 describe Banisterfiend do
-  
   describe "#name" do
-    it "should never (well) generate empty names" do
-      1000.times.map { Banisterfiend::name }.any?(&:empty?).must_equal false
+    it "it should generate a name using faker" do
+      suppress_warnings do
+        Faker::Internet = mock = MiniTest::Mock.new
+        mock.expect(:domain_word, 'meep')
+      end
+      
+      Banisterfiend::name.must_match(/meep\d{,2}/)
     end
   end
 end
